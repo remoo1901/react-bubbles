60f5e8ca896e01dfd65d432e5ed5c42981d6424c
remoo1901 <remoo1901@hotmail.com>
2020-05-15 21:21:57 -0400

    finished PUT

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { useHistory } from "react-router-dom";
+import axiosWithAuth from "../util/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -7,13 +8,16 @@ const initialColor = {
 };
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
+  
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+
+  const { push } = useHistory()
 
   const editColor = color => {
     setEditing(true);
     setColorToEdit(color);
+    console.log(color);
   };
 
   const saveEdit = e => {
@@ -21,6 +25,16 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+    .put(`/colors/${colorToEdit.id}`, colorToEdit)
+    .then(res => {
+      console.log(res.data)
+      setEditing(false)
+       push(`/bubblePage`)
+    }
+      
+      )
+    .catch((err) => console.log("ERROR", err))
   };
 
   const deleteColor = color => {
